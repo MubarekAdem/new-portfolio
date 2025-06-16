85ada97d8d92a2bea82fafce4e67527dceb73cdb
Mubarek Adem <mubarekademy@gmail.com>
2025-06-16 21:45:51 +0300

    deleted swc dependency remover

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false,
-  experimental: {
-    swcMinify: false,
-  },
-  experimental: {
-    swcMinify: false,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
